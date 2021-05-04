d1d3a015f960a2e800e02b86b2550f5a9eff3378
Ken Sodemann <ken@ionic.io>
2021-05-04 12:30:47 -0500

    chore(): update the site association

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/apple-app-site-association', (req, res) => {
-    const aasa = fs.readFileSync(path.join(__dirname, 'apple-app-site-association'));
+  .get('/.well-known/apple-app-site-association', (req, res) => {
+    const aasa = fs.readFileSync(path.join(__dirname, '.well-known/apple-app-site-association'));
     res.set('Content-Type', 'application/json');
     res.status(200).send(aasa);
   })
